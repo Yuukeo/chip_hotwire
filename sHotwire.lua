cff26c49adf50f41bb997b308625abd7c11f4336
chip <59088889+RotteRagna@users.noreply.github.com>
2019-12-30 18:03:28 +0100

    Oh yeah yeah

--- sHotwire.lua
+++ sHotwire.lua
@@ -1,1 +1,13 @@
+ESX = nil
 
+TriggerEvent('esx:getSharedObject', function(obj)
+    ESX = obj
+end)
+
+ESX.RegisterUsableItem("lockpick", function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem("lockpick", 1)
+	
+	TriggerClientEvent('chip_hotwire:hotwire', source)
+end)
